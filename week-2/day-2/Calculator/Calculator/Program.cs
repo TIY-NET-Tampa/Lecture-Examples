d8dbd65a70f16a554fbd06592dc9e00d6e27fb32
Mark Dewey <mark.t.dewey@gmail.com>
2016-11-15 14:32:52 -0500

    added basic calculaor  and method example

---
+++ week-2/day-2/Calculator/Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static int GetNumber(string message)
+        {
+            Console.WriteLine(message);
+            var num = 0;
+            var isNum = int.TryParse(Console.ReadLine(), out num);
+            if (isNum)
+            {
+                return num;
+            }
+            else
+            {
+                Console.WriteLine("Not a number");
+            }
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            var num1 = GetNumber("Give me the first number");
+           
+            Console.WriteLine("What ist he operation?");
+            var op = Console.ReadLine();
+
+            var num2 = GetNumber("Give me the second number");
+           
+            Console.WriteLine($"{num1} {op} {num2}");
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ week-2/day-2/MethodsAndFunctions/MethodsAndFunctions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndFunctions
+{
+    class Program
+    {
+
+        static void PrintSomething()
+        {
+            Console.WriteLine("something");
+        }
+
+        static void PrintSomething(string other)
+        {
+            Console.WriteLine($"something else {other}");
+        }
+
+
+
+        public void ParkCar(string carName)
+        {
+            Console.WriteLine($"Pakring {carName}");
+        }
+
+        public void ParkCar(string carName, int spacesToTake)
+        {
+            Console.WriteLine($"Parking {carName}, taking up {spacesToTake} spaces");
+        }
+
+
+
+
+        static int AddModifier(string number, int mod)
+        {
+            return (AddModifier(int.Parse(number), mod));
+        }
+
+        static int AddModifier(int number, int modifier = 3)
+        {
+            return number + modifier;
+        }
+ 
+        static void Main(string[] args)
+        {
+            PrintSomething();
+
+            PrintSomething("Hello cohort!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
