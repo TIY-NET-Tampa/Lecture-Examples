da5636d4a87799bcc6814b63f7a66ebd1cbd491d
Mark Dewey <mark.t.dewey@gmail.com>
2016-11-16 12:06:49 -0500

    added lecture example for the day

---
+++ week-2/day-2/Calculator/Calculator/Logic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Logic
+    {
+        // Add
+        public static int Add(int a, int b)
+        {
+            var rv = a + b;
+            return rv;
+        }
+        // Subtract
+        public static int Subtract(int a, int b)
+        {
+            var rv = a - b;
+            return rv;
+        }
+        // Multiple
+        public static int Multiply(int a, int b)
+        {
+            var rv = a * b;
+            return rv;
+        }
+        // Division
+        public static int Division(int a, int b)
+        {
+            var rv = a / b;
+            return rv;
+        }
+    }
+}
---
+++ week-2/day-3/SwitchInvestigation/SwitchInvestigation/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchInvestigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var guessedNumber = "test";
+            //switch (guessedNumber)
+            //{
+            //    case ("test"):
+            //        Console.WriteLine("got here for 1");
+            //        break;
+            //    case ("other"):
+            //        Console.WriteLine("got here for 2");
+            //        break;
+            //    case ("):
+            //        Console.WriteLine("got here for 3");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Nope!");
+            //        break;
+            //}
+
+            //if (guessedNumber == 1)
+            //{
+            //    Console.WriteLine("got here for 1");
+            //}
+            //else if (guessedNumber == 2)
+            //{
+            //    Console.WriteLine("got here for 2");
+            //}
+            //else if (guessedNumber == 3)
+            //{
+            //    Console.WriteLine("got here for 3");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nope!");
+            //}
+
+            // Problem: We want while, 
+            // that loops until they guess the correct number, 
+            // then it changes the display message accordingly 
+            // and breaks out of the loop
+
+            Console.Write("enter number:");
+            var guessed = int.Parse(Console.ReadLine());
+            var answer = 42;
+            var attempts = 1;
+            var maxAttempts = 5;
+
+            while (attempts <= maxAttempts)
+            {
+                
+                if (guessed == answer)
+                {
+                    Console.WriteLine("You did it");
+                }
+                else
+                {
+                    Console.WriteLine($"You have guessed {attempts} times");
+                    if (attempts > maxAttempts - 1)
+                    {
+                        Console.WriteLine("You have tried too many times");
+                    } else
+                    {
+                        Console.WriteLine("try again");
+                        guessed = int.Parse(Console.ReadLine());
+                    }
+                }
+                attempts++;
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
