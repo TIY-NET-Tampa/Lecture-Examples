b14ac8cf18321397fc39044f53befb8c2ebff3dc
Mark Dewey <mark.t.dewey@gmail.com>
2016-11-22 10:25:19 -0500

    added homework review

---
+++ week-2b/day-1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4jClient;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    public class User
+    {
+        [JsonProperty(PropertyName="name")]
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j","password");
+            client.Connect();
+
+
+            var users = client.Cypher
+                 .Match("(u:User)")
+                 .Return(u => u.As<User>())
+                 .Results;
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Name}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-2b/day-2/HomeworkReview/HomeworkReview/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkReview
+{
+    // Constructor Example
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string FurColor { get; set; }
+        public string Breed { get; set; }
+        public bool IsHappy { get; set; } = true;
+        public bool HasTail { get; set; }
+
+        public bool IsWaggingTail
+        {
+            get
+            {
+                return (this.HasTail && this.IsHappy);
+            }
+        }
+
+        public int Age { get; set; } = 0;
+
+        public Dog(string name, string furcolor, string breed)
+        {
+            Console.WriteLine("Creating a dog");
+            this.Name = name;
+            this.FurColor = furcolor;
+            this.Breed = breed;
+        }
+
+
+    }
+    
+    public class School
+    {
+        public double Teachers { get; set; }
+        public double Students { get; set; }
+        private static int GCD(int a, int b)
+        {
+            while (b > 0)
+            {
+                var rem = a % b;
+                a = b;
+                b = rem;
+            }
+            return a;
+        }
+
+        public string TeacherStudentRatio()
+        {
+            var biggest = 0d;
+            var smallest = 0d;
+            if (Teachers >= Students)
+            {
+                biggest = Teachers;
+                smallest = Students;
+            }
+            else
+            {
+                biggest = Students;
+                smallest = Teachers;
+            }
+
+            var gcd = GCD((int)smallest, (int)biggest);
+            
+            var teacher = this.Teachers / gcd;
+            var students = this.Students / gcd;
+            var rv = $"{teacher} : {students}";
+            return rv;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = 5 % 20;
+            var y = 4 % 24;
+            var z = 50 % 5;
+
+            var highSchool = new School { Students = 20, Teachers = 5 };
+            var middleSchool = new School { Students = 10, Teachers = 2 };
+            var elementary = new School { Students = 42, Teachers = 5 };
+
+            // should  == 1:4;
+            var ratio = highSchool.TeacherStudentRatio();
+
+            // should  == 1:2;
+            var ratio2 = middleSchool.TeacherStudentRatio();
+
+            // should  == 5:42;
+            var ratio3 = elementary.TeacherStudentRatio();
+
+
+            Console.WriteLine(ratio);
+
+            Console.WriteLine(ratio2);
+
+            Console.WriteLine(ratio3);
+            Console.ReadLine();
+        }
+    }
+}
