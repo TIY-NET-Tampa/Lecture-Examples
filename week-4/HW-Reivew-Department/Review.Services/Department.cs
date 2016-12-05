2d44e49a12b7a7fa6698e596d772b51df79e8c5e
Mark Dewey <mark.t.dewey@gmail.com>
2016-12-05 13:18:11 -0500

    added 4-1 notes

---
+++ week-4/HW-Reivew-Department/Review.Services/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.Services
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public Department(string name)
+        {
+            this.Name = name;
+        }
+
+
+        public void AddEmployee(string name, double salary, string phone, string email)
+        {
+            var emp = new Employee
+            {
+                Name = name,
+                Salary = salary,
+                Phone = phone,
+                Email = email
+            };
+            this.Employees.Add(emp);
+        }
+    }
+}
---
+++ week-4/HW-Reivew-Department/Review.Services/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Review.Services
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string  Email { get; set; }
+        public string Phone { get; set; }
+
+        public List<Review> Reviews { get; set; } = new List<Review>();
+
+    }
+}
