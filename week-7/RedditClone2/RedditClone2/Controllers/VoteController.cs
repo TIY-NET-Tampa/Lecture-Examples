2ac3b70a608fd2792c89a0e73af66ca49610a8c7
Mark Dewey <mark.t.dewey@gmail.com>
2017-01-06 11:05:38 -0500

    added the final example

---
+++ week-7/RedditClone2/RedditClone2/Controllers/VoteController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RedditClone2.ViewModels;
+using RedditClone2.Models;
+
+namespace RedditClone2.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/vote")]
+    public class VoteController : ApiController
+    {
+        [Route("{postId}/up")]
+        public IHttpActionResult UpVote([FromUri] int postId)
+        {
+            var db = new ApplicationDbContext();
+            var post = db.RedditPosts.FirstOrDefault(f => f.Id == postId);
+            if (post != null)
+            {
+                post.Upvotes++;
+                db.SaveChanges();
+                return Ok(new VoteResponseViewModel { NewCount = post.Upvotes - post.Downvotes});
+            }
+            return NotFound();
+        }
+
+
+        [Route("{postId}/down")]
+        public IHttpActionResult DownVote([FromUri] int postId)
+        {
+            var db = new ApplicationDbContext();
+            var post = db.RedditPosts.FirstOrDefault(f => f.Id == postId);
+            if (post != null)
+            {
+                post.Downvotes++;
+                db.SaveChanges();
+                return Ok(new VoteResponseViewModel { NewCount = post.Upvotes - post.Downvotes });
+            }
+            return NotFound();
+        }
+    }
+}
