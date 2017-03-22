31001c572d53204fba6b3391ea9ec629f84f82d4
Mark Dewey <mark.t.dewey@gmail.com>
2017-03-22 12:10:16 -0400

    notes

---
+++ week-1/ListAndDictionarys/ListAndDictionarys/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndDictionarys
+{
+    class Program
+    {
+
+        class Player
+        {
+            public string Name { get; set; }
+            public int JerseyNumber { get; set; }
+            public string Sandwich { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var myArrry = new int[5];
+            var myOtherArray = new int[] { 1, 2, 3, 4, 5 };
+            // def: var myVarible = new List<type>();
+
+            var myList = new List<string>();
+            var myOtherList = new List<double>() { 1.0, 1.2, 2.2, 3.3 };
+
+            myList.Add("4");
+            myList.Add("5");
+            myList.Add("hello");
+
+            var greeting = myList[2];
+            myList[1] = "not 5";
+
+            // we want to "hello"
+            var needle = 0;
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] == "hello")
+                {
+                    needle = i;
+                }
+
+            }
+
+            myList.RemoveAt(2);
+            myList.Remove("4");
+
+            var birthdays = new List<DateTime>();
+
+            birthdays.Add(new DateTime(1988, 12, 12));
+
+
+            // does the index change when we remove an item
+
+            var names = new List<string>()
+            {
+                "Bill","Jill", "Jim","Leigh", "Jim James", "Jim"
+            };
+
+            names.Remove("Jim");
+            var noJims = names.Where(name => name != "Jim");
+            names.Add("Not Jim");
+
+
+            var teamRoster = new Dictionary<string, Player>();
+
+            teamRoster.Add("jimmy", new Player
+            {
+                JerseyNumber = 66,
+                Name = " Jimmy",
+                Sandwich = "PB& J"
+            });
+            //teamRoster.Add("johns", 88);
+            //teamRoster.Add("betty", 14);
+            //teamRoster.Add("Sam", 99);
+
+            var jimmy = teamRoster["jimmy"];
+
+            var favoriteSandwich = jimmy.Sandwich;
+            var jimNum = jimmy.JerseyNumber;
+
+           // var bill = teamRoster["bill"]; // throw an error & crash
+
+            var player = new Player();
+            var wasInside = teamRoster.TryGetValue("bill", out player);
+
+
+            if (teamRoster.ContainsKey("bill"))
+            {
+                teamRoster["bill"] = new Player();
+            }
+            else
+            {
+                teamRoster.Add("bill", new Player());
+            }
+
+
+            // count 
+
+            var counts = new Dictionary<char, int>();
+
+            var sentence = "Big Brown Fox Jumped Over the Moon";
+
+            foreach (var character in sentence)
+            {
+                if (counts.ContainsKey(character))
+                {
+                    counts[character] += 1 ;
+                }
+                else
+                {
+                    counts.Add(character, 1);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ week-1/MethodsAndFunctions/MethodsAndFunctions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndFunctions
+{
+    class Program
+    {
+        //static returnType MethondName([any parameters,...])
+        //{
+
+        //}
+
+        static int GetValidUser()
+        {
+            var number = 0;
+            var input = Console.ReadLine();
+            var wasSuccessful = int.TryParse(input, out number);
+            while (!wasSuccessful)
+            {
+                Console.WriteLine("Try again");
+                wasSuccessful = int.TryParse(input, out number);
+            }
+            return number;
+        }
+
+        static int GetUserNumber(string putput)
+        {
+            Console.WriteLine(putput);
+            var number = GetValidUser();
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            var first = GetUserNumber("Enter First Number");
+        
+            var second = GetUserNumber("Enter Second Number");
+
+            var result = first + second;
+
+            Console.WriteLine($"Your addition is {first} + {second} = {result}");
+
+        }
+    }
+}
