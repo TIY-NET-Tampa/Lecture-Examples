606a2720257159ceb60e75284bc151c2390bd7e7
Mark Dewey <mark.t.dewey@gmail.com>
2017-03-22 12:09:44 -0400

    final notes

--- week-1/Day2HomeReview/Day2HomeReview/Program.cs
+++ week-1/Day2HomeReview/Day2HomeReview/Program.cs
@@ -15,29 +15,64 @@ namespace Day2HomeReview
 
             var counter = 0;
             var guess = 0;
-
+            var pastGuess = new int[5];
             while (counter < 5 && guess != target)
             {
                 Console.WriteLine("Give me your best guess");
                 var input = Console.ReadLine();
                 int.TryParse(input, out guess);
 
-                Console.WriteLine($"your guess was {guess}");
-
-                if (guess < target)
+                var wasAlreadyGuess = false;
+                foreach (var userGuess in pastGuess)
                 {
-                    Console.WriteLine("Too low, try again.");
+                    if (guess == userGuess)
+                    {
+                        wasAlreadyGuess = true;
+                    }
                 }
-                else if (guess > target)
+
+                if (wasAlreadyGuess)
                 {
-                    Console.WriteLine("Too high, try again");
+                    Console.WriteLine("You aalready guesses that, foool");
+                }
+                else
+                {
+                    pastGuess[counter] = guess;
+
+                    Console.WriteLine($"your guess was {guess}");
+
+                    if (guess < target)
+                    {
+                        Console.WriteLine("Too low, try again.");
+                    }
+                    else if (guess > target)
+                    {
+                        Console.WriteLine("Too high, try again");
+                    }
+
+                    Console.WriteLine("Your past guesses are:");
+                }
+                foreach (var userGuess in pastGuess)
+                {
+                    if (userGuess != 0)
+                    {
+                        Console.Write($"{userGuess},");
+                    }
                 }
                 counter++;
             }
 
-            if ()
+            if (counter > 4)
+            {
+                Console.WriteLine("You lost :-(");
+            }
+            else
+            {
+                Console.WriteLine("You Won!");
+            }
 
 
-}
+
+        }
     }
 }
