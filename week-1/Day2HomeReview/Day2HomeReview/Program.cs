c3e48e6883dcbc9a1081f32d216a4609c096d2bf
Mark Dewey <mark.t.dewey@gmail.com>
2017-03-22 09:16:18 -0400

    did the basic number guess

---
+++ week-1/Day2HomeReview/Day2HomeReview/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2HomeReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = new Random().Next(1,101);
+            Console.WriteLine($"the target is {target}");
+
+            Console.WriteLine("Give me your best guess");
+            var input = Console.ReadLine();
+            var guess = 0;
+            int.TryParse(input, out guess);
+
+            Console.WriteLine($"your guess was {guess}");
+
+
+            if (guess < target)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (guess > target)
+            {
+                Console.WriteLine("Too high");
+            }
+
+
+        }
+    }
+}
